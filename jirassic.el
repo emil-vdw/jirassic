d021e68801eab39c8903d905343b3101d44c1984
Emil van der Westhuizen <vdwemil@protonmail.com>
2025-04-28 11:52:21 +0200

    fix: set correct aio version in package requires

--- jirassic.el
+++ jirassic.el
@@ -4,7 +4,7 @@
 ;; Maintainer: Emil van der Westhuizen <vdwemil@protonmail.com>
 ;; Created: 19 April 2025
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (dash "2.0.0") (s "1.12.0") (org "9.5") (aio "1.0.0") (f "0.20.0") (request "0.3.0"))
+;; Package-Requires: ((emacs "27.1") (dash "2.0.0") (s "1.12.0") (org "9.5") (aio "1.0") (f "0.20.0") (request "0.3.0"))
 ;; Homepage: https://github.com/emil-vdw/jirassic
 ;; Keywords: tools, convenience, jira
 
